e7520900ff3f7908ed2fc1c4ab3c392063a6a6ae
manulaz81 <78950476+manulaz81@users.noreply.github.com>
2023-01-04 21:54:07 +0100

    7en1 pattern

--- app.js
+++ app.js
@@ -44,4 +44,25 @@ setInterval(function(){
 },3000);
 
 
+// $(document).ready(function(){
+//   $('.num').counterUp({
+//     time: 1200
+//   });
+// });
 
+        
+$(document).ready(function() {
+
+  $('.counter').each(function () {
+$(this).prop('Counter',0).animate({
+  Counter: $(this).text()
+}, {
+  duration: 4000,
+  easing: 'swing',
+  step: function (now) {
+      $(this).text(Math.ceil(now));
+  }
+});
+});
+
+});  
